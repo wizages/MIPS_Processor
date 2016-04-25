13674ced6f4898e3bd8f3c6f07246055177278e5
dlangerm <david.langerman@mines.sdsmt.edu>
2016-04-25 10:51:56 -0600

    finished writing case skeleton for MPU

--- MPU.vhd
+++ MPU.vhd
@@ -23,7 +23,8 @@ entity MPU is
 	PORT(
 			input_byte : in std_logic_vector(7 downto 0);
 			load_byte : in std_logic;
-			clk : in std_logic
+			clk : in std_logic;
+			reset : in std_logic
 		);
 end MPU;
 
@@ -42,18 +43,25 @@ signal pc,pc_next : unsigned(31 downto 0) := (others =>'0');
 signal ALU_done_tick , ALU_ready_tick: std_logic;
 signal ALU_result : std_logic_vector(31 downto 0) := (others =>'0');
 type MPU_states is (idle,load1,load1_wait,load_2,load2_wait,load_3,
-					load3_wait,load4,load4_wait,op,op_wait,disp1,disp2,disp3,disp4);
+					load3_wait,load4,load4_wait,op,op_wait,disp1,disp1_wait,disp2,disp2_wait,disp3,disp3_wait,disp4,disp4_wait);
 signal state, state_next : MPU_states := idle;
 
 begin
 
-process (clk)
+process (clk,reset)
 begin
 	if(rising_edge(clk)) then
 		state <= state_next;
 		pc <= pc_next;
 		reg <= reg_next;
 		instruction_reg <= instruction_reg_next;
+	end if;
+	
+	if(reset = '1') then
+		state <= idle;
+		pc <='0';
+		reg <= (others=>'0');
+		instruction_reg <= (others=>'0');
 	end if;
 end process;
 
@@ -65,7 +73,34 @@ ALU : entity work.ALU(Behavioral)
 					ready_tick => ALU_ready_tick,
 					done_tick => ALU_done_tick,
 					clk => clk);
+					
+process(state,instruction_reg,reg,pc)
+begin			
+					
+case (state) is
 
+	when idle =>
+	when load1 =>
+	when load1_wait =>
+	when load2 =>
+	when load2_wait =>
+	when load3 =>
+	when load3_wait =>
+	when load4 =>
+	when load4_wait =>
+	when op =>
+	when op_wait =>
+	when disp1=>
+	when disp1_wait=>
+	when disp2=>
+	when disp2_wait=>
+	when disp3=>
+	when disp3_wait=>
+	when disp4=>
+	when disp4_wait=>
+
+end case;
+end process;
 
 end Behavioral;
 
